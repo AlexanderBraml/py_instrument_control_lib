4475f574ddd5c5781ad99b27728f6b3c7bd87fb4
Alexander Braml <braml@fim.uni-passau.de>
2024-04-09 09:14:32 +0200

    Implement failsafe mechanism for switch matrix

--- py_instrument_control_lib/devices/SwitchMatrix.py
+++ py_instrument_control_lib/devices/SwitchMatrix.py
@@ -3,12 +3,43 @@ This code is automatically generated from '../../specifications/SwitchMatrix.jso
 Any changes made to this file are overwritten if you regenerate this module.
 Only make changes in the source file.
 """
+import socket
+import time
 
+from py_instrument_control_lib.channels.Channel import Channel
+from py_instrument_control_lib.channels.ChannelEnums import ChannelIndex
+from py_instrument_control_lib.device_base.DeviceException import DeviceException
 from py_instrument_control_lib.device_types.AbstractSwitchMatrix import AbstractSwitchMatrix
 from py_instrument_control_lib.manufacturers.FloDevice import FloDevice
 
 
 class SwitchMatrix(AbstractSwitchMatrix, FloDevice):
+
+    def query(self, query: str) -> str:
+        query += '\n' if not query.endswith('\n') else ''
+        print(query, end='')
+        self._socket.sendall(query.encode())
+        response = self._socket.recv(1024)
+
+        return response.decode()
+
+    def execute(self, command: str, ttl: int = 10, num_tries: int = 0) -> None:
+        if num_tries >= ttl:
+            raise DeviceException(msg=f'Fatal error, cannot establish connection after {ttl} attempts. Aborting.')
+
+        if num_tries > 0:
+            time.sleep(2 ** num_tries)
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                self._socket.settimeout(self._config.timeout)
+                self._socket.connect((self._config.ip, self._config.port))
+            except TimeoutError | OSError:
+                print(f'Error when trying to execute {command}. Already tried {num_tries} times.')
+
+        try:
+            self.query(command)
+        except TimeoutError | OSError:
+            self.execute(command, ttl, num_tries + 1)
 
     def set_row(self, row: int, check_errors: bool = False) \
             -> None:
@@ -36,3 +67,6 @@ class SwitchMatrix(AbstractSwitchMatrix, FloDevice):
         self.execute(f'{{"cmd": "SET_ROW_COLUMN", "row": "{row}", "column": "{col}"}}')
         if check_errors:
             self.check_error_buffer()
+
+    def get_channel(self, channel_idx: ChannelIndex, check_errors: bool = False) -> Channel:
+        raise NotImplementedError('Channels are not supported when using a switch matrix.')
